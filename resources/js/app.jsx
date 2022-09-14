68f7b6ce8411bfeb95e91af152a43b8bc85311c3
sangeethkc <keyceesangi@gmail.com>
2022-09-14 15:16:19 +0530

    React implimented

---
+++ resources/js/app.jsx
@@ -0,0 +1,8 @@
+import "./bootstrap";
+
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+import App from "./components/App";
+
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
